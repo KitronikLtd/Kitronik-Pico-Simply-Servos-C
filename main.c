65fa8a4a309dc696a7a4407eac836ced5caf0f29
JackAtKitronik <110606017+JackAtKitronik@users.noreply.github.com>
2023-05-22 08:38:42 +0100

    Removed test code from main.c

--- main.c
+++ main.c
@@ -22,51 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-/*
-#include <stdio.h>
-
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
-#include "pwm.pio.h"
-
-#define SERVO_PIN 2
-
-// Write `period` to the input shift register
-void pio_pwm_set_period(PIO pio, uint sm, uint32_t period) {
-    pio_sm_set_enabled(pio, sm, false);
-    pio_sm_put_blocking(pio, sm, period);
-    pio_sm_exec(pio, sm, pio_encode_pull(false, false));
-    pio_sm_exec(pio, sm, pio_encode_out(pio_isr, 32));
-    pio_sm_set_enabled(pio, sm, true);
-}
-
-// Write `level` to TX FIFO. State machine will copy this into X.
-void pio_pwm_set_level(PIO pio, uint sm, uint32_t level) {
-    pio_sm_put_blocking(pio, sm, level);
-}
-
-int main() {
-    stdio_init_all();
-
-    // todo get free sm
-    PIO pio = pio0;
-    int sm = 0;
-    uint offset = pio_add_program(pio, &pwm_program);
-    printf("Loaded program at %d\n", offset);
-
-    pwm_program_init(pio, sm, offset, SERVO_PIN);
-    pio_pwm_set_period(pio, sm, (1u << 16) - 1);
-
-    int level = 0;
-    while (true) {
-        printf("Level = %d\n", level);
-        pio_pwm_set_level(pio, sm, level * level);
-        level = (level + 1) % 256;
-        sleep_ms(10);
-    }
-}*/
-
-
 #include "hardware/clocks.h"
 #include "hardware/pio.h"
 
